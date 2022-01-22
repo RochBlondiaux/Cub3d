f8f023d0673ef334e796f3dc69024d48b7c28494
Roch Blondiaux <contact@roch-blondiaux.com>
2022-01-22 19:22:27 +0100

    Project base

---
+++ includes/cub3d.h
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rblondia <rblondia@student.42-lyon.fr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/22 18:39:51 by rblondia          #+#    #+#             */
+/*   Updated: 2022/01/22 19:19:24 by rblondia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+/**
+ * Libraries
+ */
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <errno.h>
+# include "mlx.h"
+
+# include "messages.h"
+
+/**
+ * Structures
+ */
+
+/**
+ * Parsing
+ */
+
+/**
+ * Utils
+ */
+
+#endif
---
+++ srcs/main.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rblondia <rblondia@student.42-lyon.fr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/22 18:39:33 by rblondia          #+#    #+#             */
+/*   Updated: 2022/01/22 19:21:27 by rblondia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/cub3d.h"
+
+int main(void)
+{
+	void	*mlx;
+	void	*mlx_win;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	mlx_loop(mlx);
+	return (EXIT_SUCCESS);
+}
